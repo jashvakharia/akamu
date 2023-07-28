49ad6eceb5304b6e61ffab675e3c7f6e5b80844b
jash <jashvakharia@gmail.com>
2023-07-28 17:53:09 +0530

    update

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 function executeJava() {
-  // Get the Java code from the text area
-  var javaCode = document.getElementById("javaCode").value;
-
-  // Create a form data object to send the code to the server
-  var formData = new FormData();
-  formData.append("javaCode", javaCode);
-
-  // Send a POST request to the server to execute the code
-  fetch("execute.php", {
-      method: "POST",
-      body: formData
-    })
+  // Send a GET request to the server to execute the Java file
+  fetch("execute.php")
     .then(function (response) {
       // Convert the response to text format
       return response.text();
